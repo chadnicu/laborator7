8bbc396822e616eec8b9b5fbe2950725835fcd9d
nicu <ontanicolae15@gmail.com>
2024-11-11 12:31:55 +0200

    Functionalitate login

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 document.getElementById('myButton').addEventListener('click', function () {
-    alert('Button was clicked!');
+    login();
 });
+
+function login() {
+    const username = prompt("Enter your username:");
+    const password = prompt("Enter your password:");
+
+    if (username === "amps" && password === "12345678") {
+        alert("Login successful!");
+    } else {
+        alert("Invalid username or password.");
+    }
+}
